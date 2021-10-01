75fa2f433355f4fd627ed18177ea6015a42488d9
sgp1986 <sgp1229@gmail.com>
2021-09-30 21:13:19 -0600

    updating rps bonus

--- small_programs/rps_bonus.rb
+++ small_programs/rps_bonus.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 score = {
   'player' => 0,
   'computer' => 0,
@@ -18,51 +20,64 @@ def prompt(message)
   puts "=> #{message}"
 end
 
-def user_choice(input)
-  VALID_CHOICES.include?(input)
-end
-
-def who_won?(player_move, computer_move, winning_choice)
-  if winning_choice[player_move].include?(computer_move)
+def who_won?(player_move, computer_move, winner)
+  if winner[player_move].include?(computer_move)
     'player'
-  elsif winning_choice[computer_move].include?(player_move)
+  elsif winner[computer_move].include?(player_move)
     'computer'
   else
     'no one'
   end
 end
 
-loop do
-  choice = ''
+def s_or_s(move)
+  prompt 'Do you mean scissors or spock?'
   loop do
-    prompt "Choose one: #{WINNING_CHOICE.keys.join(', ')}"
-    choice = gets.chomp.downcase
-
-    if choice == 's'
-      prompt 'Do you mean scissors or spock?'
-      choice = gets.chomp.downcase
-      if choice.start_with?('sc')
-        choice = 'scissors'
-      elsif choice.start_with?('sp')
-        choice = 'spock'
-      else
-        puts 'invalid choice.'
-      end
+    move = gets.chomp.downcase
+    if move.start_with?('sc')
+      move = 'scissors'
+      break
+    elsif move.start_with?('sp')
+      move = 'spock'
+      break
     else
+      puts 'invalid choice.'
+    end
+  end
+  move
+end
+
+def valid_choice?(choice)
+  loop do
+    if 
       VALID_CHOICES.each do |selection|
         if selection.start_with?(choice)
           choice = selection
         end
       end
+    break
+    else prompt "Invalid input, try again."
     end
-    break if user_choice(choice)
-    prompt "Invalid input, try again."
   end
+  choice
+end
+
+loop do
+  choice = ''
+ 
+    prompt "Choose one: #{WINNING_CHOICE.keys.join(', ')}"
+    choice = gets.chomp.downcase
+
+    if choice == 's'
+      s_or_s(choice)
+    end
+
+    choice = valid_choice?(choice)
 
   computer_choice = WINNING_CHOICE.keys.sample
 
   winner = who_won?(choice, computer_choice, WINNING_CHOICE)
-
+  binding.pry
   score[winner] += 1
 
   prompt "You chose: #{choice}; the computer chose: #{computer_choice}"
