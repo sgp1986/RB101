3d2abef040ee8c2f3ef45a4833ddbb3c84387afe
sgp1986 <sgp1229@gmail.com>
2021-10-22 15:19:16 -0600

    letter_counter

--- exercises/easy5/letter_counter.rb
+++ exercises/easy5/letter_counter.rb
@@ -30,16 +30,17 @@
 #   p counts
 # end
 
-def word_sizes(string)
+def word_sizes(words_string)
   counts = Hash.new(0)
-  string.split.each do |word|
-    counts[word.size] += 1
+  words_string.split.each do |word|
+    clean_word = word.delete('^A-Za-z')
+    counts[clean_word.size] += 1
   end
-  counts
+  p counts
 end
 
-word_sizes('Four score and seven.') == {4=>1, 5=>1, 3=>1, 6=>1}
-# word_sizes('Hey diddle diddle, the cat and the fiddle!') == { 3 => 5, 6 => 1, 7 => 2 }
-# word_sizes("What's up doc?") == { 6 => 1, 2 => 1, 4 => 1 }
-# word_sizes('') == {}
+word_sizes('Four score and seven.') == { 3 => 1, 4 => 1, 5 => 1, 6 => 1 }
+word_sizes('Hey diddle diddle, the cat and the fiddle!') == { 3 => 5, 6 => 1, 7 => 2 }
+word_sizes("What's up doc?") == { 6 => 1, 2 => 1, 4 => 1 }
+word_sizes('') == {}
 
